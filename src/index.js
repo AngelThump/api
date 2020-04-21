57be643d268e2ea4076ea60dca42bec6875f7e9f
TimIsOverpowered <admin@angelthump.com>
2020-04-21 07:03:57 -0500

    prod/localhost refactors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@
 const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const server = app.listen(port, 'localhost');
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+server.on('listening', () => {
+    if (process.env.NODE_ENV === "production") {
+      logger.info('Feathers application production build started on http://%s:%d', app.get('host'), port)
+    } else {
+      logger.info('Feathers application development build started on http://%s:%d', app.get('host'), port)
+    }
+  }
 );
